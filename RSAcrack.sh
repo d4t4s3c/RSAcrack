6ddbb495c2ecb50c46a95e189d2ad42c9d3cf994
d4t4s3c <d4t4s3c@protonmail.com>
2021-06-03 16:05:20 +0200

    UPDATAE

--- RSAcrack.sh
+++ RSAcrack.sh
@@ -78,10 +78,10 @@ fi
 while read password; do
     ssh-keygen -y -f $key -P $password &>/dev/null
         if [ $? -eq 0 ]; then
-        	echo -e "$GreenLight[$CyanLight*$GreenLight]$White Craking:$CyanLight  $key"
-        	sleep 2
-        	echo -e "$GreenLight[$CyanLight*$GreenLight]$White Wordlist:$CyanLight $wordlist"
-        	sleep 2
+            echo -e "$GreenLight[$CyanLight*$GreenLight]$White Craking:$CyanLight  $key"
+            sleep 2
+            echo -e "$GreenLight[$CyanLight*$GreenLight]$White Wordlist:$CyanLight $wordlist"
+            sleep 2
             echo -e "$GreenLight[$Red+$GreenLight]$Red Password:$GreenLight $password"
             sleep 4
             exit 0
