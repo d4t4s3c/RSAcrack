0e83e2603b83d86dbec879592907da61bd37f93d
Miguel R <d4t4s3c@protonmail.com>
2021-10-11 09:07:52 +0200

    UPDATE

--- RSAcrack.sh
+++ RSAcrack.sh
@@ -29,6 +29,7 @@ declare -r var17='Error!'
 declare -r var18='Fuck!'
 declare -r var19='it was not possible to crack his key'
 declare -r var20='SSH-Keygen not installed'
+declare -r var21='Status:'
 declare -r wordlist="$1"
 declare -r key="$2"
 
@@ -61,7 +62,19 @@ function banner(){
 }
 
 function main(){
+        echo ""
         echo -e "$White$var1$YellowLight$var3$White$var2$Red $var12 $White$var9 $Red$var6$White$var10$Red$var5 $Red$var6$White$var11$Red$var5$End"
+        echo ""
+}
+
+function info(){
+        echo ""
+        echo -e "$White$var1$Red$var7$White$var2 $White$var13 $CyanLight$key$End"
+        sleep 1
+        echo -e "$White$var1$Red$var7$White$var2 $White$var14 $CyanLight$wordlist$End"
+        sleep 1
+        echo -e "$White$var1$YellowLight$var3$White$var2$Red$White $var21$End"
+        sleep 1
 }
 
 if [ ! -z $wordlist ]; then
@@ -77,24 +90,32 @@ fi
 
 if [ ! -z $key ]; then
         /usr/bin/chmod 600 $key &>/dev/null
+        info
 else
         main
         exit 0
 fi
 
+#progress
+lines=$(/usr/bin/wc -l $wordlist)
+regex="([0-9]+).$wordlist"
+[[ $lines =~ $regex ]]
+siz="${BASH_REMATCH[1]}"
+
 while read password; do
+                line=$((line + 1))
+                progress=$((line * 100 / siz))
+                echo -ne "\r$YellowLight    $line/$fileSize ($progress%) ($password) $End"
                 /usr/bin/ssh-keygen -y -f $key -P $password &>/dev/null
         if [ $? -eq 0 ]; then
-                echo -e "$White$var1$Red$var7$White$var2 $White$var13 $CyanLight$key"
-                sleep 2
-                echo -e "$White$var1$Red$var7$White$var2 $White$var14 $CyanLight$wordlist"
-                sleep 2
                 f1=$(/usr/bin/cat $wordlist | /usr/bin/grep "^$password$" -n | /usr/bin/cut -d: -f1)
-                echo -e "$White$var1$GreenLight$var8$White$var2 $Red$var15 $GreenLight$password$Red $var16 $GreenLight$f1"
-                sleep 4
+                echo -e "\n$White$var1$GreenLight$var8$White$var2 $Red$var15 $GreenLight$password$Red $var16 $GreenLight$f1"
+                echo ""
+                sleep 2
                 exit 0
         fi
 done < $wordlist
-echo -e "$White$var1$Red$var4$White$var2 $Red$var18 $White$var19$End"
-sleep 4
+echo -e "\r$White$var1$Red$var4$White$var2 $Red$var18 $White$var19$End"
+echo ""
+sleep 2
 exit 0
